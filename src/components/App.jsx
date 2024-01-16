20ebaf4a2cf8161a86b4106854240f3b459ac45c
Debeluk <popcorn.starinets@gmail.com>
2024-01-16 14:19:28 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
-import { fetchContacts } from '../redux/contactsSlice'
+import { fetchContacts } from '../redux/contactsSlice';
 
 const App = () => {
   useEffect(() => {
